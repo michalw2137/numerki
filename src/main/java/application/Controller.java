b5a1d8b1fb50f797fee08784c8d1b194316bc65a
wrzemian <236706@edu.p.lodz.pl>
2022-03-14 22:54:12 +0100

    most TODOs done

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -10,37 +10,10 @@ import java.util.Scanner;
 public class Controller {
 
     public static void startApp() throws IOException {
-        System.out.println("CHOOSE FUNCTION:");
-        System.out.println("1) " + new FunctionPolynomial().getFormula());
-        System.out.println("2) " + new FunctionExponential().getFormula());
-        System.out.println("3) " + new FunctionSine().getFormula());
-        System.out.println("4) " + new FunctionCosine().getFormula());
-        System.out.println("5) " + new FunctionTangent().getFormula());
-
-        System.out.print("Your choice (1-5): ");
 
         Function function;
-        int choice  = System.in.read();
-        switch (choice - 48) {
-            case 1 -> {
-                function = new FunctionPolynomial();
-            }
-            case 2 -> {
-                function = new FunctionExponential();
-            }
-            case 3 -> {
-                function = new FunctionSine();
-            }
-            case 4 -> {
-                function = new FunctionCosine();
-            }
-            case 5 -> {
-                function = new FunctionTangent();
-            }
-            default -> {
-                throw new RuntimeException("Choice out of scope!"); // TODO: keep asking to enter a number until correct
-            }
-        }
+
+        function = chooseFunction();
 
         var arguments = function.calculateArgumentsIntoList(function.getGraphLeft(), function.getGraphRight());
         var values = function.calculateValuesIntoList(function.getGraphLeft(), function.getGraphRight());
@@ -52,20 +25,28 @@ public class Controller {
 
         double left, right;
         Scanner scan = new Scanner( System.in );
-        System.out.print("Enter left edge:"); // TODO: check, if edges are in graph scope?
-        left = scan.nextDouble();
-        System.out.print("Enter left edge:"); // TODO: check, if right > left?
-        right = scan.nextDouble();
-        System.out.println("left = " + left + ", right = " + right);
-        System.out.println();
+        do {
+            do {
+                do {
+                    System.out.print("Enter left edge:");
+                    left = scan.nextDouble();
+                } while (left < function.getGraphLeft());
+                do {
+                    System.out.print("Enter right edge:");
+                    right = scan.nextDouble();
+                } while (right > function.getGraphRight());
+            } while (left > right);
+            System.out.println("left = " + left + ", right = " + right);
+            System.out.println();
+        }while(function.fun(left) * function.fun(right) > 0);
 
-        if (function.fun(left) * function.fun(right) > 0) {
-            throw new RuntimeException("Edges are the same sign!"); // TODO: keep asking for edges until signs differ
-        }
-
-        double solution = Bisection.Iterations(function, left, right, 10);  // TODO: ask for iterations number
+        System.out.println("Enter number of iterations:");
+        int iterations = scan.nextInt();
+        double solution = Bisection.Iterations(function, left, right, iterations);
         System.out.println("Solution found in 10 iterations = " + solution);
-        solution = Bisection.Approximity(function, left, right, 0.001);      // TODO: ask for Epsilon
+        System.out.println("Enter Epsilon:");
+        double epsilon = scan.nextDouble();
+        solution = Bisection.Approximity(function, left, right, epsilon);
         System.out.println("Solution found when delta reached 0.001 = " + solution);
         System.out.println();
 
@@ -73,10 +54,48 @@ public class Controller {
 
         // TODO: possibly loop program for multiple inputs, quit on user input
         // TODO: split into functions?
-        // TODO: implement makeshift javafx GUI instead of console?
 
         // TODO: implement another algorithm
         // TODO: store/process results in a friendly way, check report requirements
     }
+
+    static Function chooseFunction() throws IOException {
+        System.out.println("CHOOSE FUNCTION:");
+        System.out.println("1) " + new FunctionPolynomial().getFormula());
+        System.out.println("2) " + new FunctionExponential().getFormula());
+        System.out.println("3) " + new FunctionSine().getFormula());
+        System.out.println("4) " + new FunctionCosine().getFormula());
+        System.out.println("5) " + new FunctionTangent().getFormula());
+
+        System.out.print("Your choice (1-5): ");
+        boolean repeat = false;
+        do {
+            int choice = System.in.read();
+            switch (choice - 48) {
+                case 1 -> {
+                    return new FunctionPolynomial();
+                }
+                case 2 -> {
+                    return new FunctionExponential();
+                }
+                case 3 -> {
+                    return new FunctionSine();
+                }
+                case 4 -> {
+                    return new FunctionCosine();
+                }
+                case 5 -> {
+                    return new FunctionTangent();
+                }
+                default -> {
+                    System.out.println("CHOOSE CORRECT ONE!");
+                    System.out.println("Your choice (1-5): ");
+                    repeat = true;
+                }
+            }
+        }while (repeat);
+        return new FunctionPolynomial();
+    }
+
 
 }
