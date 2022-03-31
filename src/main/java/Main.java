49dfbf8c32edfd42f0e800e67094cb5fec5b48f5
wrzemian <236706@edu.p.lodz.pl>
2022-03-31 13:16:54 +0200

    menu zad3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,36 @@
 import application.ControllerZad1;
 import application.ControllerZad2;
+import application.ControllerZad3;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {    // działa jeden na raz, odkomentowuj pojedynczo
-//         while (true) {
-//            try
-//            {
-//                ControllerZad1.startApp();
-//            }
-//            catch (RuntimeException e)
-//            {
-//                System.out.println("Program ended by user");
-//                return;
-//            }
-//        }
-        System.out.println("Type 'q' to exit \n");
-        while (true) {
-            try {
-                ControllerZad2.startApp();
-            } catch (RuntimeException | IOException e) {
-                System.out.println(e.getMessage());
-//                e.printStackTrace();
-                System.out.println();
-            } catch (IllegalAccessException e) {
-                System.out.println(e.getMessage());
+         while (true) {
+            try
+            {
+                ControllerZad3.startApp();
+            }
+            catch (RuntimeException e)
+            {
+                System.out.println("Program ended by user");
                 return;
             }
         }
+//        System.out.println("Type 'q' to exit \n");
+//        while (true) {
+//            try {
+//                ControllerZad2.startApp();
+//            } catch (RuntimeException | IOException e) {
+//                System.out.println(e.getMessage());
+////                e.printStackTrace();
+//                System.out.println();
+//            } catch (IllegalAccessException e) {
+//                System.out.println(e.getMessage());
+//                return;
+//            }
+//        }
 
     }
 
---
+++ src/main/java/application/ControllerZad3.java
@@ -0,0 +1,125 @@
+package application;
+
+import functions.*;
+import numeric_methods.Bisection;
+import numeric_methods.Secant;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ControllerZad3 {
+
+    private static double left, right;
+    private static Function function;
+    private static int nodesNumber;
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void startApp() throws RuntimeException{
+        chooseFunction();
+
+        readEdges();
+
+        readNodesNumber();
+
+        
+
+        System.out.println();
+    }
+
+    private static void readNodesNumber() {
+        System.out.print("Enter nodes number: ");
+        nodesNumber = scanner.nextInt();
+        
+        System.out.println("\nNodes number: " + nodesNumber );
+        System.out.println();
+    }
+
+    static void chooseFunction() throws RuntimeException{
+        System.out.println("CHOOSE FUNCTION:");
+        System.out.println( "1) " + new FunctionPolynomial().getFormula() + "\n" +
+                "2) " + new FunctionExponential().getFormula() + "\n" +
+                "3) " + new FunctionSine().getFormula() + "\n" +
+                "4) " + new FunctionCosine().getFormula() + "\n" +
+                "5) " + new FunctionTangent().getFormula()  + "\n" +
+                "9) exit \n\n" +
+                "Your choice (1-5): ");
+        while(true) {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    function = new FunctionPolynomial();
+                    return;
+                }
+                case 2 -> {
+                    function = new FunctionExponential();
+                    return;
+                }
+                case 3 -> {
+                    function = new FunctionSine();
+                    return;
+                }
+                case 4 -> {
+                    function = new FunctionCosine();
+                    return;
+                }
+                case 5 -> {
+                    function = new FunctionTangent();
+                    return;
+                }
+                case 9 -> {
+                    throw new RuntimeException("Program ended by user");
+                }
+                default -> {
+                    System.out.println( "CHOOSE CORRECT ONE!" + '\n' +
+                            "Your choice (1-5): ");
+                }
+            }
+        }
+    }
+
+    private static void readEdges() {
+        while (true) {
+            try {
+                tryToReadEdges();
+                return;
+            } catch (IOException | RuntimeException e) {
+                System.out.println(e.getMessage() + '\n');
+            }
+        }
+    }
+
+    private static void tryToReadEdges () throws IOException, RuntimeException {
+        System.out.print("Enter left edge: ");
+        left = scanner.nextDouble();
+
+        System.out.print("Enter right edge: ");
+        right = scanner.nextDouble();
+
+        if (left >= right) {
+            throw new IOException("Invalid range!");
+        }
+        if( function.fun(left) * function.fun(right) > 0) {
+            throw new RuntimeException("Same sign edges!");
+        }
+
+        System.out.println("\nselected range: <" + left + ", " + right + ">");
+        System.out.println();
+
+    }
+
+   
+
+    public static void setFunction (Function function) {
+        ControllerZad3.function = function;
+    }
+
+    public static void setRange(double left, double right) {
+        if( function.fun(left) * function.fun(right) > 0) {
+            throw new RuntimeException("Same sign edges!");
+        }
+        ControllerZad3.left = left;
+        ControllerZad3.right = right;
+    }
+
+}
